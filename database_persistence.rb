e511095bc21f6c0a5b7e48dc61e306b9c48d3697
Garashi <lucasbonner@protonmail.com>
2021-09-17 16:14:48 -0500

    Very minor additions, also wanted to try branches

---
+++ database_persistence.rb
@@ -0,0 +1,22 @@
+require "pg"
+
+class DatabasePersistence
+  def initialize(logger)
+    @db = if Sinatra::Base.production?
+            PG.connect(ENV['DATABASE_URL'])
+          else
+            PG.connect(dbname: "todos")
+          end
+
+    @logger = logger
+  end
+
+  def disconnect
+    @db.close
+  end
+
+  def query(statement, *params)
+    @logger.info "#{statement}: #{params}"
+    @db.exec_params(statement, params)
+  end
+end
