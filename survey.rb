860012cf697daf00bdc0e6bb3e10cfcc677bf172
Garashi <lucasbonner@protonmail.com>
2021-09-30 14:51:01 -0500

    Added email webpage and verification (thru regex). Also little session identifier to signify that who is logged in

--- survey.rb
+++ survey.rb
@@ -7,10 +7,23 @@ configure do
   set :session_secret, 'secret'
 end
 
-before do
-  session[:surveys] ||= []
+configure(:development) do
+  require "sinatra/reloader"
+  also_reload "database_persistence.rb"
 end
 
+before do
+  session[:surveys] ||= []
+  session[:email] ||= []
+end
+
+helpers do
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  def check_email(email_address)
+    ((email_address =~ VALID_EMAIL_REGEX) == 0)
+  end
+end
 
 get "/" do
   redirect "/surveys"
@@ -20,7 +33,34 @@ end
 get "/surveys" do
   @surveys = session[:surveys]
 
+  if session[:email].empty?
+    redirect "/email"
+  end
+
   erb :surveys
+end
+
+#Email form
+get "/email" do
+  erb :email
+end
+
+post "/email" do
+  email = params["email"]
+
+  if !(check_email(email))
+    erb :email
+    session[:error] = "Please enter a valid email address"
+  elsif session[:email].empty?
+    session[:email] << params["email"]
+    session[:success] = "Email added successfully"
+  else
+    session[:email].clear
+    session[:email] << params["email"]
+    session[:success] = "Email added successfully"
+  end
+
+  redirect "/"
 end
 
 #New survey form
